90db9381063a92442c33c0b6bf0bdcaebe592836
Jeroen Schmit <jeroen@slimndap.com>
2015-05-29 11:08:20 +0200

    The validaiton no longer checks if a field is required, since Gravity Forms already handles this. Fixes AdmiumNL/adm-gravity-iban#2

--- adm-gravity-iban.php
+++ adm-gravity-iban.php
@@ -31,32 +31,35 @@ if ( class_exists( 'GFForms' ) ) {
         return $script;
         
     }
-    
-    add_filter("gform_validation", "custom_validation");
-    function custom_validation($validation_result){
-        
-        foreach($validation_result["form"]["fields"] as &$field){
-            
-            $fieldValue = rgpost("input_{$field['id']}");
 
-            if ($field['inputMaskValue'] == "iban"){
-                if (strlen($fieldValue) == 0 && $field['isRequired'] != 1){ // If empty and field is optional continue in foreach loop
-                    continue;
-                } else if (strlen($fieldValue) == 0 && $field['isRequired'] == 1){ // if empty and field is required
-                    $validation_result["is_valid"] = false;
-                    $field["failed_validation"] = true;
-                    $field["validation_message"] = __("This field is required.", "gravityforms");
-                } else { // If not empty do the IBAN check
-                    require_once("php-iban.php");
-                    if(!verify_iban($fieldValue)) {
-                        $validation_result["is_valid"] = false;
-                        $field["failed_validation"] = true;
-                        $field["validation_message"] = __("Please enter a valid value.", "gravityforms");
-                    }
-                }
-            }
-        }
-        return $validation_result;
-        
-    }
+	/**
+	 * Validates Gravity Forms fields with an IBAN mask.
+	 *
+	 * @since 	0.1
+	 * @param 	array 	$validation_result	Contains the validation result and the current Form Object.
+	 * @return 	array						The new validation result.
+	 */
+	function gform_iban_validation($validation_result){
+
+	    foreach ( $validation_result['form']['fields'] as &$field ) {
+
+	        $fieldValue = rgpost( "input_{$field['id']}" );
+
+	        if ( 'iban' == $field['inputMaskValue'] ) {
+				if ( 0 == strlen( $fieldValue ) ) { // If empty continue in foreach loop
+	                continue;
+	            } else { // If not empty do the IBAN check
+					require_once( dirname( __FILE__ ) . '/php-iban.php' );
+	                if ( ! verify_iban( $fieldValue ) ) {
+	                    $validation_result['is_valid'] = false;
+	                    $field['failed_validation'] = true;
+	                    $field['validation_message'] = __( 'Please enter a valid value.' , 'gravityforms' );
+	                }
+	            }
+	        }
+	    }
+	    return $validation_result;
+
+	}
+	add_filter( 'gform_validation', 'gform_iban_validation' );
 }
